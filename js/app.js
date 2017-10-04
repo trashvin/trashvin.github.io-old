ec79e75d795eedd6ee954102976a099d6c02bae8
Trashvin <m3lles@gmail.com>
2017-10-04 17:31:10 +0800

    V1.0.2

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function fillData() {
+    document.getElementById('main_title').innerHTML = main_title;
+    document.getElementById('main_title_name').innerHTML=main_name;
+    document.getElementById('main_sub_title').innerHTML= main_sub_title;
+    document.getElementById('footer_main').innerHTML= footer_text;
+    document.getElementById('footer').innerHTML= footer_text;
+    document.getElementById('about_content_1').innerHTML = about_content_1;
+    document.getElementById('about_content_2').innerHTML = about_content_2;
+    document.getElementById('skill_intro').innerHTML = skill_intro;
+    document.getElementById('project_intro').innerHTML = project_intro;
+    document.getElementById('contact_intro').innerHTML = contact_intro;
+}
