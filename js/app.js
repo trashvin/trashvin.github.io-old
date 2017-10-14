3ff06e621ab849af84c2ed6fe102a42ef8690319
Trashvin <m3lles@gmail.com>
2017-10-14 21:40:51 +0800

    Refactor and fix blog link

--- js/app.js
+++ js/app.js
@@ -1,66 +1,62 @@
 function fillData() {
-    document.getElementById('main_title').innerHTML = main_title;
-    document.getElementById('main_title_name').innerHTML=main_name;
-    document.getElementById('main_sub_title').innerHTML= main_sub_title;
-    document.getElementById('footer').innerHTML= footer_text;
-    document.getElementById('about_content_1').innerHTML = about_content_1;
-    document.getElementById('about_content_2').innerHTML = about_content_2;
-    document.getElementById('skill_intro_1').innerHTML = skill_intro_1;
-    //document.getElementById('skills_footer').innerHTML = skills_footer;
-    document.getElementById('project_intro_1').innerHTML = project_intro_1;
-    document.getElementById('project_intro_2').innerHTML = project_intro_2;
-    document.getElementById('contact_intro').innerHTML = contact_intro;
-    document.getElementById('last_update').innerHTML = last_update;
-    document.getElementById('footer_m').innerHTML= footer_text;
+  document.getElementById("main_title").innerHTML = main_title;
+  document.getElementById("main_title_name").innerHTML = main_name;
+  document.getElementById("main_sub_title").innerHTML = main_sub_title;
+  document.getElementById("footer").innerHTML = footer_text;
+  document.getElementById("about_content_1").innerHTML = about_content_1;
+  document.getElementById("about_content_2").innerHTML = about_content_2;
+  document.getElementById("skill_intro_1").innerHTML = skill_intro_1;
+  //document.getElementById('skills_footer').innerHTML = skills_footer;
+  document.getElementById("project_intro_1").innerHTML = project_intro_1;
+  document.getElementById("project_intro_2").innerHTML = project_intro_2;
+  document.getElementById("contact_intro").innerHTML = contact_intro;
+  document.getElementById("last_update").innerHTML = last_update;
+  document.getElementById("footer_m").innerHTML = footer_text;
 
-    var line = document.createElement("br");
-    for(var skill in skills) {
-        var top_div = document.createElement("div");
-        var top_title = document.createElement('span');
-        top_title.style = "font-weight:bold";
-        top_title.innerHTML = skills[skill] + '<br/>';
-        top_div.style ="background:whitesmoke";
-        top_div.id = skill;
-        top_div.appendChild(top_title);
-        //top_div.appendChild(line);
-        
-        document.getElementById('skills_list').appendChild(top_div);
+  var line = document.createElement("br");
+  for (var skill in skills) {
+    var top_div = document.createElement("div");
+    var top_title = document.createElement("span");
+    top_title.style = "font-weight:bold";
+    top_title.innerHTML = skills[skill] + "<br/>";
+    top_div.style = "background:whitesmoke";
+    top_div.id = skill;
+    top_div.appendChild(top_title);
+    //top_div.appendChild(line);
 
-        var sub_skills = skill_list[skill];
-        if (sub_skills != null) {
-            for(var key in sub_skills) {
-                
-                var main = document.createElement("span");
-                var inner = document.createElement("span");
-                main.className='label';
-                main.innerHTML = key;
-                inner.className='tooltiptext';
-                if ( sub_skills[key] == 0) {
-                    inner.innerHTML = "Short project or on going study/training"
-                    main.appendChild(inner);
-                } else if( sub_skills[key]> 0){
-                    inner.innerHTML= sub_skills[key].toString() + " year/s";
-                    main.appendChild(inner);
-                } else {
+    document.getElementById("skills_list").appendChild(top_div);
 
-                }
-                top_div.appendChild(main);        
-            }
-        }        
+    var sub_skills = skill_list[skill];
+    if (sub_skills != null) {
+      for (var key in sub_skills) {
+        var main = document.createElement("span");
+        var inner = document.createElement("span");
+        main.className = "label";
+        main.innerHTML = key;
+        inner.className = "tooltiptext";
+        if (sub_skills[key] == 0) {
+        inner.innerHTML = "Short project or on going study/training";
+        main.appendChild(inner);
+        } else if (sub_skills[key] > 0) {
+        inner.innerHTML = sub_skills[key].toString() + " year/s";
+        main.appendChild(inner);
+        } else {
+        }
+        top_div.appendChild(main);
+      }
     }
-    var footer = document.createElement('span');
-    footer.innerHTML = skills_footer;
-    document.getElementById('skills_list').appendChild(line);
-    document.getElementById('skills_list').appendChild(footer);
-    
-    
+  }
+  var footer = document.createElement("span");
+  footer.innerHTML = skills_footer;
+  document.getElementById("skills_list").appendChild(line);
+  document.getElementById("skills_list").appendChild(footer);
 }
 
 function myFunction() {
-    var x = document.getElementById("topnav_id");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("topnav_id");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
--- js/values.js
+++ js/values.js
@@ -1,120 +1,146 @@
-var main_title ='Hello, I am'
-var main_name  ='Marvin'
-var main_sub_title='Software Engineer / Application Developer'
+var main_title = "Hello, I am";
+var main_name = "Marvin";
+var main_sub_title = "Software Engineer / Application Developer";
 
-var about_content_1 = 'I am Marvin, a software developer based in the Phillipines. I design and develop software applications for any platform. I have been working in this exciting field for more than 14 years. Developed robust '
-about_content_1 = about_content_1 + 'and user interface intuitive applications for small clients, significant information system for a school or university, prototype applications for new technologies, and critical production application '
-about_content_1 = about_content_1 + 'for large retail chain. I have been involved into every phase of the software lifecycle and have work with diverse team both foreign and local. Consequently, I have kept abreast with the latest technology, patterns '
-about_content_1 = about_content_1 + 'and trend in the industry. Always looking forward to venture into new and exciting projects utilizing emerging technologies, frameworks, architectures, and strategies.'
+var about_content_1 =
+  "I am Marvin, a software developer based in the Phillipines. I design and develop software applications for any platform. I have been working in this exciting field for more than 14 years. Developed robust ";
+about_content_1 =
+  about_content_1 +
+  "and user interface intuitive applications for small clients, significant information system for a school or university, prototype applications for new technologies, and critical production application ";
+about_content_1 =
+  about_content_1 +
+  "for large retail chain. I have been involved into every phase of the software lifecycle and have work with diverse team both foreign and local. Consequently, I have kept abreast with the latest technology, patterns ";
+about_content_1 =
+  about_content_1 +
+  "and trend in the industry. Always looking forward to venture into new and exciting projects utilizing emerging technologies, frameworks, architectures, and strategies.";
 
-var about_content_2 = 'During my leasure time I am fond of reading blogs and articles from reputable online sources. I binge on Netflix '
-about_content_2 = about_content_2 + 'and  Spotify. I maintain a huge playlist and used to play guitar and ukelele. I am a sport junkie, follows '
-about_content_2 = about_content_2 + ' the Boston Red Sox, New England Patriots, and Boston Celtics every season. I maintain a list of blogs, writing '
-about_content_2 = about_content_2 + 'just about anything, from politics, sports, new apps, tips and tricks to creating tutorials. Lastly, I do not start the day without a coffee.'
+var about_content_2 =
+  "During my leasure time I am fond of reading blogs and articles from reputable online sources. I binge on Netflix ";
+about_content_2 =
+  about_content_2 +
+  "and  Spotify. I maintain a huge playlist and used to play guitar and ukelele. I am a sport junkie, follows ";
+about_content_2 =
+  about_content_2 +
+  " the Boston Red Sox, New England Patriots, and Boston Celtics every season. I maintain a list of blogs, writing ";
+about_content_2 =
+  about_content_2 +
+  "just about anything, from politics, sports, new apps, tips and tricks to creating tutorials. Lastly, I do not start the day without a coffee.";
 
-var skill_intro_1= '"Design is a way of life, a point of view. It involves the whole complex of visual communications: talent, creative ability, manual skill, and technical knowledge. Aesthetics and economics, technology and psychology are intrinsically related to the process." - Paul Rand'
-var skill_intro_2='For years I have used and learned the following skill sets.'
-var skills_footer='Click on each skill for more information. The list will be updated as it fits.'
-var project_intro_1 = 'Organization and process is the way the I do things. Phases are vital to an organized project. Requirements gathering,'
-var project_intro_1 = project_intro_1 + 'design, proof of concepts, actual development, testing, implementation an maintenance is a must. Clients will always be '
-var project_intro_1 = project_intro_1 + 'consulted on the design. I make sure that the code are elegant and maintainable, and that the industries best practices are '
-var project_intro_1 = project_intro_1 + 'followed. User acceptance is a key step before any application is deployed in production. When the system is live, support'
-var project_intro_1 = project_intro_1 + 'is always available ensuring the resolution of bugs in a timely manner.'
-var project_intro_2 = 'List of projects, both work and personal, are currently being collected and a high level descriptions will be posted once available.'
-var contact_intro ='Feel free to drop me a note by email or leave a message on one of the social mediums I am available'
-
-var last_update ='October 2017'
-var footer_text ='A Trillescope Project &copy 2017'
+var skill_intro_1 =
+  '"Design is a way of life, a point of view. It involves the whole complex of visual communications: talent, creative ability, manual skill, and technical knowledge. Aesthetics and economics, technology and psychology are intrinsically related to the process." - Paul Rand';
+var skill_intro_2 =
+  "For years I have used and learned the following skill sets.";
+var skills_footer =
+  "Click on each skill for more information. The list will be updated as it fits.";
+var project_intro_1 =
+  "Organization and process is the way the I do things. Phases are vital to an organized project. Requirements gathering,";
+var project_intro_1 =
+  project_intro_1 +
+  "design, proof of concepts, actual development, testing, implementation an maintenance is a must. Clients will always be ";
+var project_intro_1 =
+  project_intro_1 +
+  "consulted on the design. I make sure that the code are elegant and maintainable, and that the industries best practices are ";
+var project_intro_1 =
+  project_intro_1 +
+  "followed. User acceptance is a key step before any application is deployed in production. When the system is live, support";
+var project_intro_1 =
+  project_intro_1 +
+  "is always available ensuring the resolution of bugs in a timely manner.";
+var project_intro_2 =
+  "List of projects, both work and personal, are currently being collected and a high level descriptions will be posted once available.";
+var contact_intro =
+  "Feel free to drop me a note by email or leave a message on one of the social mediums I am available";
+var last_update = "October 2017";
+var footer_text = "A Trillescope Project &copy 2017";
 
 var skills = {
-    "languages":"Programming Languages",
-    "frameworks":"Frameworks",
-    "databases":"Databases",
-    "sources":"Source Controls",
-    "cis":"Continuous Integration Tools",
-    "devs":"Development Tools",
-    "os":"Operating Systems",
-    "others":"Others"
+  languages: "Programming Languages",
+  frameworks: "Frameworks",
+  databases: "Databases",
+  sources: "Source Controls",
+  cis: "Continuous Integration Tools",
+  devs: "Development Tools",
+  os: "Operating Systems",
+  others: "Others"
 };
 var languages = {
-    "C#":15,
-    "VB.NET":8,
-    "SQL":10,
-    "Phyton":3,
-    "Java":2,
-    "JavaScript":2,
-    "TypeScript":1,
-    "C++":1,
-    "Swift":0,
-    "ES6":0
+  "C#": 15,
+  "VB.NET": 8,
+  SQL: 10,
+  Phyton: 3,
+  Java: 2,
+  JavaScript: 2,
+  TypeScript: 1,
+  "C++": 1,
+  Swift: 0,
+  ES6: 0
 };
 var frameworks = {
-    ".NET Framework":15
-    ,"ADO.NET":10,
-    "Entity Framework":4,
-    "ASP.NET":3,
-    "WPF":2,
-    "Android":2,
-    ".NET Core/Standard":1,
-    "jQuery":1,
-    "Angular":1,
-    "BottlePy":1,
-    "PyGame":1,
-    "Flask":0,
-    "Xamarin":0,
-    "UWP":0,
-    "React":0,
-    "VueJS":0
+  ".NET Framework": 15,
+  "ADO.NET": 10,
+  "Entity Framework": 4,
+  "ASP.NET": 3,
+  WPF: 2,
+  Android: 2,
+  ".NET Core/Standard": 1,
+  jQuery: 1,
+  Angular: 1,
+  BottlePy: 1,
+  PyGame: 1,
+  Flask: 0,
+  Xamarin: 0,
+  UWP: 0,
+  React: 0,
+  VueJS: 0
 };
 var databases = {
-    "MS SQL": 13,
-    "MySQL / MariaDB" : 3,
-    "MongoDB":2,
-    "MS Access":2,
-    "Oracle":0
+  "MS SQL": 13,
+  "MySQL / MariaDB": 3,
+  MongoDB: 2,
+  "MS Access": 2,
+  Oracle: 0
 };
 var sources = {
-    "MKS / PTC":5,
-    "Git":3,
-    "TFS / VSTS":2,
-    "SourceOffsite":1,
-    "SVN":1
+  "MKS / PTC": 5,
+  Git: 3,
+  "TFS / VSTS": 2,
+  SourceOffsite: 1,
+  SVN: 1
 };
 var cis = {
-    "VSTS / TFS": 1,
-    "AppVeyor": 2,
-    "Travis": 1
+  "VSTS / TFS": 1,
+  AppVeyor: 2,
+  Travis: 1
 };
 var devs = {
-    "Visual Studio":15,
-    "Eclipse":2,
-    "Visual Studio Code":2,
-    "Android Studio":1,
-    "PyCharm": 2,
-    "XCode": 0
+  "Visual Studio": 15,
+  Eclipse: 2,
+  "Visual Studio Code": 2,
+  "Android Studio": 1,
+  PyCharm: 2,
+  XCode: 0
 };
 var os = {
-    "Microsoft Windows":15,
-    "Mac OS": 3,
-    "Android": 4,
-    "iOS":3,
-    "Linux":3,
-    "Microft Windows Server":2,
+  "Microsoft Windows": 15,
+  "Mac OS": 3,
+  Android: 4,
+  iOS: 3,
+  Linux: 3,
+  "Microft Windows Server": 2
 };
 var others = {
-    "HTML":10,
-    "CSS":5,
-    "Photoshop":5,
-    "MS Expression Blend":-1
-}
+  HTML: 10,
+  CSS: 5,
+  Photoshop: 5,
+  "MS Expression Blend": -1
+};
 var skill_list = {
-    "languages":languages,
-    "frameworks":frameworks,
-    "databases":databases,
-    "sources":sources,
-    "cis":cis,
-    "devs":devs,
-    "others":others,
-    "os":os
-    
+  languages: languages,
+  frameworks: frameworks,
+  databases: databases,
+  sources: sources,
+  cis: cis,
+  devs: devs,
+  others: others,
+  os: os
 };
